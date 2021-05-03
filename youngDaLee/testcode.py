1cd8d907421ef38f17fcb0aee2db96d61d066194
YoungDaLee <ldy0956@gmail.com>
2021-05-03 18:38:20 +0900

    ch08

--- youngDaLee/testcode.py
+++ youngDaLee/testcode.py
@@ -1,36 +1,17 @@
-
-from collections import deque
-
 n, m = map(int, input().split())
+money = list(map(int, input().split()))
 
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input())))
+dp = [10001]*(m+1) # 최댓값으로 dp 채워넣음
+dp[0] = 0
 
-# 상하좌우
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
 
-def bfs(x, y):
-    queue = deque() # 큐 생성
-    queue.append((x, y))
+for i in money:
+    for j in range(i, m+1):
+        if dp[j-i] != 10001:
+            dp[j] = min(dp[j], dp[j-i]+1)
 
-    while queue: # 큐가 빌 때 까지
-        x, y = queue.popleft()
 
-        # 상하좌우 확인
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if nx < 0 or nx >= n or ny < 0 or ny >= m: # 범위넘으면
-                continue
-            if graph[nx][ny] == 0: # 괴물 있으면
-                continue
-            if graph[nx][ny] == 1: # 괴물 없고 처음 방문하면
-                graph[nx][ny] = graph[x][y] + 1 # 최단거리 기록
-                queue.append((nx, ny))
-    
-    return graph[n-1][m-1] #(n,m) 최단거리 반환
-
-print(bfs(0,0))
+if dp[m] == 10001:
+    print(-1)
+else:
+    print(dp[m])
