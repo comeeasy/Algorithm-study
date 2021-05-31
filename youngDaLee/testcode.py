eae4573d26916f12d2c400c09a65ed660c85d50d
YoungDaLee <ldy0956@gmail.com>
2021-05-31 18:16:34 +0900

    ch12

--- youngDaLee/testcode.py
+++ youngDaLee/testcode.py
@@ -1,16 +1,23 @@
-def solution(food_times, k):
-    answer = k%len(food_times) # 모든 음식이 충분할 경우 돌아오는 위치
-    cycle = k//len(food_times) # 회전 수
+n = int(input())
+k = int(input())
 
-    print(answer, cycle)
-    for i in range(len(food_times)):
-        f = food_times[i]
-        if f < cycle: # 회전 수 보다 음식 수가 적으면
-            if i >= answer:
-                answer += (cycle-f+1)
-            else:
-                answer += (cycle-f)
-    
-    answer = (answer%len(food_times)) +1
-    return answer
-print(solution([3, 1, 2], 5))
+board = []
+## board = [[0]*n]*n 이렇게 하니까 값 변경할 때 잘 안됨
+board.append([1]*(n+2))
+for i in range(n):
+    li = []
+    for j in range(n+2):
+        if j == 0 or j == (n+1):li.append(1) # 외벽
+        else: li.append(0)
+    board.append(li)
+board.append([1]*(n+2))
+
+for i in range(k):
+    x, y = map(int,input().split())
+    board[x][y] = 2 # 사과 위치 표시
+
+
+l = int(input())
+
+
+print(board)
